3137b448cbe48a7c8992a0b0f3e3a26c73fbcbc7
alexoa <alex00yt@gmail.com>
2023-12-05 18:10:25 -0600

    improve pdf being downloaded

--- src/views/MainSection.vue
+++ src/views/MainSection.vue
@@ -79,7 +79,7 @@
                 <use href="@/assets/icons.svg#github" />
               </svg>
             </a>
-            <a href="/cv_alexoa.pdf" download>
+            <a href="/cv_alexoa.pdf" target="_blank">
               <svg>
                 <use href="@/assets/icons.svg#resume" />
               </svg>
